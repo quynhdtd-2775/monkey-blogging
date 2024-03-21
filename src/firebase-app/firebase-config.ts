1346ff736dbf26bf2ae3608f04958f7b4d864266
quynhdtd-2775 <doan.thi.diem.quynh@sun-asterisk.com>
2024-03-21 15:28:55 +0700

    Delele API key

--- src/firebase-app/firebase-config.ts
+++ src/firebase-app/firebase-config.ts
@@ -16,14 +16,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-
-  // apiKey: "AIzaSyBDgfGL29zdYw662yWXztBMA_4jBvC6A-g",
-  // authDomain: "fir-monkey-blogging.firebaseapp.com",
-  // projectId: "fir-monkey-blogging",
-  // storageBucket: "fir-monkey-blogging.appspot.com",
-  // messagingSenderId: "716370907066",
-  // appId: "1:716370907066:web:705f0dd21c5e83da24d783",
-  // measurementId: "G-4J3VXJ7C6E",
 };
 
 // Initialize Firebase
